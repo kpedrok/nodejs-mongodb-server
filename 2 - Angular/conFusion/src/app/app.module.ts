6c3477f2509bb00d3e045ee14d4d8fced286ffee
kpedrok <pedrokvb@gmail.com>
2020-03-11 20:32:39 -0300

    First commit

--- 2 - Angular/conFusion/src/app/app.module.ts
+++ 2 - Angular/conFusion/src/app/app.module.ts
@@ -1,25 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatListModule} from '@angular/material/list'
 
 import { AppComponent } from './app.component';
-import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    FlexLayoutModule,
-    MatListModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
