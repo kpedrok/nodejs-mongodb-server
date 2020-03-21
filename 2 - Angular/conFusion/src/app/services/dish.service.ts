d62d0367afced90dc407bfade1d569fa8a163180
kpedrok <pedrokvb@gmail.com>
2020-03-21 19:41:58 -0300

    Single Page Applications Part 2

--- 2 - Angular/conFusion/src/app/services/dish.service.ts
+++ 2 - Angular/conFusion/src/app/services/dish.service.ts
@@ -18,6 +18,6 @@ export class DishService {
   }
 
   getFeaturedDish(): Dish {
-    return DISHES.filter((dish) => (dish.featured))[0];
+    return DISHES.filter((dish) => dish.featured)[0];
   }
 }
