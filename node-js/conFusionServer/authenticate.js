2f697706116efc5ffb21cd8fa7145fb6007a6539
Pedro Boas <pedrokvb@gmail.com>
2021-04-18 16:51:27 -0300

    Passport Facebook

--- node-js/conFusionServer/authenticate.js
+++ node-js/conFusionServer/authenticate.js
@@ -4,15 +4,13 @@ var User = require('./models/users');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config.js');
-
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
-
 
 exports.getToken = function (user) {
   return jwt.sign(user, config.secretKey,
@@ -49,3 +47,30 @@ exports.verifyAdmin = function (req, res, next) {
     return res.send('You are not authorized to perform this operation!');
   }
 };
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+  clientID: config.facebook.clientId,
+  clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOne({ facebookId: profile.id }, (err, user) => {
+    if (err) {
+      return done(err, false);
+    }
+    if (!err && user !== null) {
+      return done(null, user);
+    }
+    else {
+      user = new User({ username: profile.displayName });
+      user.facebookId = profile.id;
+      user.firstname = profile.name.givenName;
+      user.lastname = profile.name.familyName;
+      user.save((err, user) => {
+        if (err)
+          return done(err, false);
+        else
+          return done(null, user);
+      })
+    }
+  });
+}
+));
--- node-js/conFusionServer/config.js
+++ node-js/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/conFusion'
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientId: '933834377410048',
+    clientSecret: 'de95a8efbb2d4c7ff0467eb836527c36'
+  }
 }
